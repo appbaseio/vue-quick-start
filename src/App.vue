f47b0362c5133c135865265bd509119b37ae4d7f
bietkul <kuldepsaxena155@gmail.com>
2023-01-24 13:44:02 +0530

    feat: add aggs

--- src/App.vue
+++ src/App.vue
@@ -27,18 +27,42 @@
           }
         ]"
       />
+      <multi-list
+        componentId="Authors"
+        dataField="authors.keyword"
+        title="Popular Authors"
+        :aggregationSize="5"
+      />
+      <single-range
+        componentId="Ratings"
+        dataField="average_rating"
+        defaultValue="All Books"
+        :data="[
+          { start: 0, end: 5, label: 'All Books' },
+          { start: 4, end: 5, label: '4 stars and up' },
+          { start: 3, end: 5, label: '3 stars and up' },
+        ]"
+        title="Book Ratings"
+      />
 		</reactive-base>
 	</div>
 </template>
 
 <script>
-import { ReactiveBase, SearchBox } from '@appbaseio/reactivesearch-vue';
+import { 
+  ReactiveBase, 
+  SearchBox, 
+  MultiList, 
+  SingleRange
+} from '@appbaseio/reactivesearch-vue';
 
 export default {
   name: 'App',
   components: {
     ReactiveBase,
     SearchBox,
+    MultiList,
+    SingleRange
   }
 }
 </script>
