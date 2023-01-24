2a153e2c900845f24d0702edceaf05c133302247
bietkul <kuldepsaxena155@gmail.com>
2023-01-24 13:40:22 +0530

    add search

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,40 @@
 			app="good-books-ds"
 			credentials="04717bb076f7:be54685e-db84-4243-975b-5b32ee241d31"
 		>
-			<h1>Hello from ReactiveBase 👋</h1>
+      <search-box
+        componentId="SearchBox"
+        placeholder="Search for books or authors"
+        :dataField="[
+          {
+            'field': 'authors',
+            'weight': 3
+          },
+          {
+            'field': 'authors.autosuggest',
+            'weight': 1
+          },
+          {
+            'field': 'original_title',
+            'weight': 5
+          },
+          {
+            'field': 'original_title.autosuggest',
+            'weight': 1
+          }
+        ]"
+      />
 		</reactive-base>
 	</div>
 </template>
 
 <script>
-import { ReactiveBase } from '@appbaseio/reactivesearch-vue';
+import { ReactiveBase, SearchBox } from '@appbaseio/reactivesearch-vue';
 
 export default {
   name: 'App',
   components: {
-    ReactiveBase
+    ReactiveBase,
+    SearchBox,
   }
 }
 </script>
